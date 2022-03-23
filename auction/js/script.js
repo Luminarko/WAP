2757377e84d839df8a9cdbb9939743238314fb42
Imrich Zeleňák <92807683+Luminarko@users.noreply.github.com>
2022-03-23 10:36:01 +0100

    Update script.js

--- auction/js/script.js
+++ auction/js/script.js
@@ -1,9 +1,18 @@
 const gallery = document.getElementById("gallery");
+const details = document.getElementById("details");
 
 for(let index in data){
     console.log(index);
     let item = data[index];
-    let p = document.createElement("div");
-    p.innerHTML = `<h2>${item.name}</h2><p>• ${item.description}</p><p>• ${item.author}</p>`;
-    gallery.appendChild(p);
+    let itemElement = document.createElement("div");
+    itemElement.innerHTML = `<img src="${item.images[0]}"><h2 id="name">${item.name}</h2><p> ${item.description}</p><p> ${item.author}</p><p class="price">${item.price}</p>`;
+    gallery.appendChild(itemElement);
+    
+    itemElement.onclick = () => {
+        details.innerHTML = `<div class="background"><div class="deta"><img src="${item.images}"><h2 id="name">${item.name}</h2><p> ${item.description}</p><p> ${item.author}</p><p class="price">${item.price}</p></div></div>`;
+        details.style.display = "flex";
+    };
+    details.onclick = () => {
+        details.style.display = "none";
+    };
 }
